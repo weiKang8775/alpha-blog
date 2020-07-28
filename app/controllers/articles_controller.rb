05061f5e3c7599e4ba3670e3b3652ccd8d91bc2f
Wei Kang <wei.kang8775@gmail.com>
2020-07-28 09:40:25 -0600

    Add edit and update actions and the associated view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,12 +8,32 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+
+
   end
 end
