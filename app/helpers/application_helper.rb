2da15d2834fc8c2f48a7a81471770f742408abf1
Wei Kang <wei.kang8775@gmail.com>
2020-08-02 20:55:16 -0600

    Add users profile picture

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(user, size = 64)
+    email_address = user.email.downcase
+    hash = hash = Digest::MD5.hexdigest(email_address)
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: "#{user.username}'s profile picture", class: "rounded mx-auto d-block shadow")
+  end
 end
