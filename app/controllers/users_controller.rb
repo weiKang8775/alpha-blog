9f9f8c32a05e078f9c5f52775b25945bbd7d887a
Wei Kang <wei.kang8775@gmail.com>
2020-08-04 16:38:50 -0600

    Add sessions and clean up code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if (@user.update(user_params))
       flash[:notice] = "Your profile is updated successfully."
       redirect_to user_path(@user)
@@ -29,6 +28,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if (@user.save)
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to Alpha Blog! You have successfully signed up."
       redirect_to user_path(@user)
     else
@@ -37,10 +37,16 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @user.destroy
+    redirect_to root_path
   end
 
   private
   def user_params
     params.require(:user).permit(:email, :username, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
